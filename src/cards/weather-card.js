0c1ac1b5b3acecedd2545d6365a08a92f87d9a38
Raph Santana <raphy@raphsant.me>
2021-04-09 17:24:00 -0500

    Weather component done

--- src/cards/weather-card.js
+++ src/cards/weather-card.js
@@ -3,22 +3,42 @@ import React, { useState, useEffect } from "react";
 function WeatherCard() {
   const [weatherData, setWeatherData] = useState(null);
   const apiKey = "f0427030a3eaff2558613297e9184af8";
-  const url = `api.openweathermap.org/data/2.5/weather?q=chicago&appid=${apiKey}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=chicago&units=metric&appid=${apiKey}`;
 
-  const getWeather = async () => {
-    useEffect(() => {
-        try {
-            const weather = await fetch(url);
-            const weatherJson = await weatherJson.json();
-        } catch (err) {
-            console.error(err);
-        }
-    }, []);
-  };
+  useEffect(() => {
+    const getWeather = async () => {
+      try {
+        const weather = await fetch(url);
+        const json = await weather.json();
+        console.log(json);
+        setWeatherData(json);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getWeather();
+    console.log("weather component mounted.");
+  }, []);
 
-  return <div>
-      
-  </div>;
+  let weatherContent;
+
+  if (weatherData !== null) {
+    weatherContent = (
+      <div>
+        <h2>{weatherData.name}</h2>
+        <h3>
+          {Math.round(weatherData.main.temp)}° degrees (Feels like{" "}
+          {Math.round(weatherData.main.feels_like)}°)
+        </h3>
+        <h4>
+          {Math.round(weatherData.main.temp_min)}° |{" "}
+          {Math.round(weatherData.main.temp_max)}°
+        </h4>
+      </div>
+    );
+  }
+
+  return <div>{weatherContent}</div>;
 }
 
 export default WeatherCard;
